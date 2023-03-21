5c14946167c41d4b9e8e096f25396b67c760f8df
jainsparsh75 <jainsparsh75@gmail.com>
2023-03-22 00:30:17 +0530

    .ebextenstions and application.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ def index():
     return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
-# use get method
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
